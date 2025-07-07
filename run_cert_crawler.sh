a21ceef5c006f602ff65ed47c57d1f4e799bcf62
Hemanth-5 <165142873+Hemanth-5@users.noreply.github.com>
2025-07-07 10:03:35 +0000

    Implement FID user access validation and ownership change for installation files

--- run_cert_crawler.sh
+++ run_cert_crawler.sh
@@ -4,7 +4,7 @@ set -e
 PLAYBOOK=windows_cert_crawler_install.yml
 INVENTORY=hosts.ini
 
-EXTRA_VARS="fid='22z225@psgtech.ac.in' installation_path='C:/Temp/CertCrawler/' operation_mode='opmode1' cert1_dir_path='C:/Temp/CertCrawler/test-certs/' cert1_types='p12,cer' extract_root_certs='Y'"
+EXTRA_VARS="fid='HEMANTH/testuser2' installation_path='C:/Temp/CertCrawler/' operation_mode='opmode1' cert1_dir_path='C:/Temp/CertCrawler/test-certs/' cert1_types='p12,cer' extract_root_certs='Y'"
 
 echo "Running ansible-playbook with variables:"
 echo $EXTRA_VARS
